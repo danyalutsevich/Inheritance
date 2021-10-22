3a887fe3c3dde7d7b5b90f60680ce0eca77066b5
danyalutsevich <luchevich31@gmail.com>
2021-10-22 23:47:37 +0300

    Magazine&Booklet

---
+++ inheritance/Factory.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "Library.h"
+
+class LitFactory {
+
+private:
+
+	size_t parts;
+
+	std::string* splitString(std::string str, char sym) {
+
+		size_t pos = 0;
+		parts = 1;
+
+		while ((pos = str.find(sym, pos + 1)) != std::string::npos) {
+			parts++;
+		}
+
+		std::string* res = new std::string[parts];
+		pos = 0;
+		size_t pos2;
+
+		for (int i = 0; i < parts - 1; i++) {
+
+			pos2 = str.find(sym, pos + 1);
+			res[i] = str.substr(pos, pos2 - pos);
+			pos = pos2;
+
+
+		}
+
+		res[parts - 1] = str.substr(pos + 1);
+
+
+
+		return res;
+	}
+
+public:
+
+
+	Literature* fromString(std::string str) {
+
+		
+		//splitt for lines
+		auto arr = splitString(str, '\n');
+
+		size_t lines = parts;
+
+		if (lines < 2) {
+
+			return NULL;
+		}
+
+		//split :
+
+
+
+
+
+
+		auto pair1 = splitString(arr[0], ':');
+		auto pair2 = splitString(arr[1], ':');
+
+
+
+
+
+		if (lines == 2) {
+
+			if (pair2[0] == "author") {
+
+				Book* book = new Book;
+				return book->SetAuthor(pair2[1])->SetTitle(pair1[1]);
+
+			}
+			if (pair2[0] == "number") {
+
+				Journal* journal = new Journal;
+				return journal->SetNumber(stoi(pair2[1]))->SetTitle(pair1[1]);
+
+			}
+			if (pair2[0] == "number") {
+
+				Journal* journal = new Journal;
+				return journal->SetNumber(stoi(pair2[1]))->SetTitle(pair1[1]);
+
+			}
+		}
+		if (lines == 3) {
+
+			auto pair3 = splitString(arr[2], ':');
+			if (pair3[0] == "date") {
+
+				Booklet* booklet = new Booklet;
+				return booklet->SetDate(pair3[1])->SetAuthor(pair2[1])->SetTitle(pair1[1]);
+
+			}
+			if (pair3[0] == "year") {
+
+				Magazine* mag = new Magazine;
+				return mag->SetYear(stoi(pair3[1]))->SetNumber(stoi(pair2[1]))->SetTitle(pair1[1]);
+
+			}
+		}
+
+
+
+
+		return NULL;
+
+	}
+
+
+
+};
--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -1,101 +1,19 @@
 
 #include <iostream>
 #include "Library.h"
-
+#include "Factory.h"
 
 
 int main()
 {
 
-	Book* book = new Book;
-	book->SetAuthor("author")->SetTitle("book");
-
-	Journal * journal = new Journal;
-	journal->SetNumber(1)->SetTitle("journal");
 	
-	Newspaper* newspaper = new Newspaper;
-	newspaper->SetDate("date")->SetTitle("newspaper");
-
-	Booklet* booklet = new Booklet;
-	booklet->SetDate("date")->SetAuthor("author")->SetTitle("booklet");
-
-	Magazine* mag = new Magazine;
-	mag->SetYear(2077)->SetNumber(0)->SetTitle("magazine");
-
-	Magazine* mag1 = new Magazine;
-	mag1->SetYear(2078)->SetNumber(1)->SetTitle("magazine one");
-
-	Magazine* mag2 = new Magazine;
-	mag2->SetYear(2079)->SetNumber(2)->SetTitle("magazine two");
-
-	Literature** funds = new Literature*[7];
-
-	funds[0] = book;
-	funds[1] = mag;
-	funds[2] = journal;
-	funds[3] = mag1;
-	funds[4] = newspaper;
-	funds[5] = mag2;
-	funds[6] = booklet;
-
-
-	for (int i = 0; i < 7; i++) {
-
-		std::cout << funds[i]->toString() << "\n";
-
-	}
-
-	std::cout << "\n\n";
-
-	/*for (int i = 0; i < 7; i++) {
-		
-		Book* ptr = dynamic_cast<Book*>(funds[i]);
-
-		if (ptr != NULL) {
-			std::cout << ptr->GetAuthor()<< " " << ptr->GetTitle() << "\n";
-
-		}
-	}
-
-	for (int i = 0; i < 7; i++) {
-
-		Newspaper* ptr = dynamic_cast<Newspaper*>(funds[i]);
-
-		if (ptr != NULL) {
-			std::cout << ptr->GetDate() << " " << ptr->GetTitle() << "\n";
-
-		}
-	}
-
-	for (int i = 0; i < 7; i++) {
-
-		Journal* ptr = dynamic_cast<Journal*>(funds[i]);
-
-		if (ptr != NULL) {
-			std::cout << ptr->GetNumber() << " " << ptr->GetTitle() << "\n";
-
-		}
-	}*/
-
-	for (int i = 0; i < 7; i++) {
-
-		Childish* ptr = dynamic_cast<Childish*>(funds[i]);
-
-		if (ptr != NULL) {
-			std::cout <<funds[i]->toString() << "\n";
-
-		}
-	}
-
-
-
-	delete mag;
-	delete booklet;
-	delete newspaper;
-	delete journal;
-	delete book;
-	delete[] funds;
-	return 0;
-
-}
+	LitFactory factory;
+	std::string str = "title:Quantum mechanics\nnumber:7\nyear:3";
+	Literature *fund=factory.fromString(str);
+	std::cout << fund->toString() << "\n";
+	delete fund;
+	
+	
+}	
 
