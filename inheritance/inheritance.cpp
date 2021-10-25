468475a8f59ce56541056767e4391814155b2bdd
danyalutsevich <luchevich31@gmail.com>
2021-10-25 19:24:24 +0300

    Factory with files

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -18,27 +18,26 @@ int main()
 	if (dynamic_cast<Magazine*>(fund)) {
 
 		delete dynamic_cast<Magazine*>(fund);
-
 	}
 	else if (dynamic_cast<Booklet*>(fund)) {
 
 		delete dynamic_cast<Booklet*>(fund);
-
 	}
 	else if (dynamic_cast<Newspaper*>(fund)) {
 
 		delete dynamic_cast<Newspaper*>(fund);
-
 	}
 	else if (dynamic_cast<Journal*>(fund)) {
 
 		delete dynamic_cast<Journal*>(fund);
-
 	}
 	else if (dynamic_cast<Book*>(fund)) {
 
 		delete dynamic_cast<Book*>(fund);
+	}
+	else {
 
+		delete fund;
 	}
 
 
